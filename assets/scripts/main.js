e65d1e3dcbdfcf4d9da5cde12de4147e0fd598c5
anupamroy8 <anupamroy8@gmail.com>
2020-08-06 00:12:38 +0530

    done till level characters

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -31,3 +31,71 @@ class Player {
 }
 
 Player.prototype.size = new Vec(0.8, 1.5);
+
+// Lava
+
+class Lava {
+  constructor(position, speed, reset) {
+    this.position = position;
+    this.speed = speed;
+    this.reset = reset;
+  }
+  get type() {
+    return "lava";
+  }
+  static create(position, char) {
+    let speed = {};
+    switch (char) {
+      case "=":
+        speed = new Vec(2, 0);
+        return new Lava(position, speed);
+      case "|":
+        speed = new Vec(0, 2);
+        return new Lava(position, speed);
+      case "v":
+        speed = new Vec(0, 3);
+        return new Lava(position, speed, position);
+      default:
+        alert("Invalid lava symbol!");
+    }
+  }
+}
+
+Lava.prototype.size = new Vec(1, 1);
+
+// Coin
+
+class Coin {
+  constructor(position, basePosition, wobble) {
+    this.position = position;
+    this.basePosition = basePosition;
+    this.wobble = wobble;
+  }
+  get type() {
+    return "coin";
+  }
+  static create(position) {
+    const basePosition = position.plus(new Vec(0.2, 0.1));
+    const wobble = 2 * Math.PI * Math.random();
+    return new Coin(basePosition, basePosition, wobble);
+  }
+}
+
+Coin.prototype.size = new Vec(0.6, 0.6);
+
+// actors
+
+const actors = {
+  "@": Player,
+  o: Coin,
+  "|": Lava,
+  "=": Lava,
+  v: Lava,
+};
+
+const background = {
+  "#": "wall",
+  "+": "lava",
+};
+
+//
