cd0c88961dc3f38bf27d627142e4223257e318cb
anupamroy8 <anupamroy8@gmail.com>
2020-08-05 12:50:58 +0530

    vector and player class added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,33 @@
-console.log("test");
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(next) {
+    return new Vec(this.x + next.x, this.y + next.y);
+  }
+
+  times(factor) {
+    return new Vec(this.x * factor, this.y * factor);
+  }
+}
+
+// console.log(new Vec(1, 2));
+
+// player
+
+class Player {
+  constructor(position, speed) {
+    this.position = position;
+    this.speed = speed;
+  }
+  get type() {
+    return "player";
+  }
+  static create(position) {
+    return new Player(position.plus(new Vec(0, -5)), new Vec(0, 0));
+  }
+}
+
+Player.prototype.size = new Vec(0.8, 1.5);
